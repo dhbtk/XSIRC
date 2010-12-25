942fd5f710975cd80f7c242fccff086badfcc1fc
NieXS <neo.niexs@gmail.com>
2010-12-25 20:10:04 -0200

    Thread safety for gen_timestamp under Linux

--- src/gen_timestamp.c
+++ src/gen_timestamp.c
@@ -4,7 +4,12 @@
 /* Cross-platform timestamp generation, since Time.local uses localtime_r, which
    is not available under MinGW since Win32's localtime is thread-safe */
 char *gen_timestamp(char *format,time_t time) {
+#ifdef WINDOWS
+	struct tm *timestruct = malloc(sizeof(struct tm));
+	localtime_r(&time,timestruct);
+#else
 	struct tm *timestruct = localtime(&time);
+#endif
 	char out[200];
 	strftime(out,sizeof(out),format,timestruct);
 	return strdup(out);
