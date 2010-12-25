423e0a5ff984a5962751e457183f6dedb7fdc922
NieXS <neo.niexs@gmail.com>
2010-12-25 20:13:45 -0200

    More thread-safety for *nix in gen_timestamp

--- src/gen_timestamp.c
+++ src/gen_timestamp.c
@@ -7,10 +7,15 @@ char *gen_timestamp(char *format,time_t time) {
 #ifdef WINDOWS
 	struct tm *timestruct = malloc(sizeof(struct tm));
 	localtime_r(&time,timestruct);
-#else
-	struct tm *timestruct = localtime(&time);
-#endif
 	char out[200];
 	strftime(out,sizeof(out),format,timestruct);
 	return strdup(out);
+#else
+	struct tm *timestruct = localtime(&time);
+	char *out = malloc(200*sizeof(char));
+	strftime(out,200*sizeof(char),format,timestruct);
+	char *result = strdup(out);
+	free(out);
+	return result;
+#endif
 }
