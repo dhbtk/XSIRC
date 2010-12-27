00d94d134bf45e560605ecfd5c63b820cd82d697
Eduardo Niehues <neo.niexs@gmail.com>
2010-12-26 22:05:06 -0200

    Fixed gen_timestamp

--- src/gen_timestamp.c
+++ src/gen_timestamp.c
@@ -8,14 +8,14 @@
    is not available under MinGW since Win32's localtime is thread-safe */
 char *gen_timestamp(char *format,time_t time) {
 #ifdef WINDOWS
-	struct tm *timestruct = malloc(sizeof(struct tm));
-	localtime_r(&time,timestruct);
+	struct tm *timestruct = localtime(&time);
 	char out[200];
 	strftime(out,sizeof(out),format,timestruct);
 	return strdup(out);
 #else
-	struct tm *timestruct = localtime(&time);
 	char *out = malloc(200*sizeof(char));
+	struct tm *timestruct = malloc(sizeof(struct tm));
+	localtime_r(&time,timestruct);
 	strftime(out,200*sizeof(char),format,timestruct);
 	char *result = strdup(out);
 	free(out);
