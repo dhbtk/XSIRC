f4fed67c041d030a308ae8698bf5f557e5a59273
Simon Lindholm <simon.lindholm10@gmail.com>
2011-06-17 01:20:08 +0800

    Simplified the timestamp generation

--- src/gen_timestamp.c
+++ src/gen_timestamp.c
@@ -10,20 +10,15 @@
 /* Cross-platform timestamp generation, since Time.local uses localtime_r, which
    is not available under MinGW since Win32's localtime is thread-safe */
 char *gen_timestamp(char *format,time_t time) {
-#ifdef WINDOWS
-	struct tm *timestruct = localtime(&time);
 	char out[200];
-	strftime(out,sizeof(out),format,timestruct);
-	return strdup(out);
+	struct tm timestruct;
+#ifdef WINDOWS
+	timestruct = *localtime(&time);
 #else
-	char *out = malloc(200*sizeof(char));
-	struct tm *timestruct = malloc(sizeof(struct tm));
-	localtime_r(&time,timestruct);
-	strftime(out,200*sizeof(char),format,timestruct);
-	char *result = strdup(out);
-	free(out);
-	return result;
+	localtime_r(&time,&timestruct);
 #endif
+	strftime(out,sizeof(out),format,&timestruct);
+	return strdup(out);
 }
 
 #ifdef WINDOWS
